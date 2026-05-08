d88571923048c6cac0d7881228e1cb74968d9570
Mayarí Achondo <mayari.achondo.parra@estudiante.ipss.cl>
2026-05-08 17:00:06 -0400

    fix: set login as initial route

--- .history/app/_layout_20260508161029.tsx
+++ .history/app/_layout_20260508161029.tsx
@@ -3,11 +3,14 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
+export const unstable_settings = {
+  anchor: "(tabs)",
+};
+
 export default function RootLayout() {
   return (
     <ThemeProvider value={DefaultTheme}>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
